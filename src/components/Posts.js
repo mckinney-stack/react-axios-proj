35d158e7a7e2e4fe3eec58213b874b9b2e92b2c9
Evan McKinney-Robinson <evan.mckinney-robinson@theverygroup.com>
2024-10-02 18:31:00 +0100

    Implement delete post functionality

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getPosts } from '../services/postService';
+import { getPosts, deletePost } from '../services/postService';
 
 export default function Posts() {
 
@@ -15,12 +15,26 @@ export default function Posts() {
         });
     }, []);
 
+    const handleDelete = (id) => {
+        deletePost(id)
+        .then(() => {
+            setPosts(posts.filter((post) => post.id !== id));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
     return (
         <div>
             <h1>Posts</h1>
             <ul>
                 {posts.map((post) => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <button onClick={() => handleDelete(post.id)}>Delete</button>
+                    </li>
                 ))}
             </ul>
         </div>
